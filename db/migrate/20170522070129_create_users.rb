4a6147c29dc5f498b15a9fff05b0b43933689369
Nhan Nguyen <nhannvt@vccvn.com>
2017-06-14 08:36:34 +0700

    Change sch

--- db/migrate/20170522070129_create_users.rb
+++ db/migrate/20170522070129_create_users.rb
@@ -1,13 +1,20 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table "user", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "Table account" do |t|
-	    t.string   "user_name",  limit: 24,  null: false
-	    t.string   "password",   limit: 32,  null: false
+	    t.string   "user_name",  limit: 24,              null: false
+	    t.string   "password",   limit: 128,             null: false
 	    t.string   "name",       limit: 128
-	    t.date "birthday"
+	    t.date     "birthday"
 	    t.datetime "joined_at"
 	    t.datetime "created_at"
 	    t.datetime "updated_at"
+	    t.string   "salt",       limit: 128
+	    t.string   "last_login", limit: 45
+	    t.integer  "level",      limit: 1
+	    t.integer  "manager_id"
+	    t.integer  "status",     limit: 1,   default: 0
+	    t.string   "job",        limit: 256
+	    t.string   "phone",      limit: 45
 	    t.index ["user_name"], name: "user_name_UNIQUE", unique: true, using: :btree
 	  end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 20170522070129) do
 
   create_table "user", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "Table account" do |t|
-    t.string   "user_name",  limit: 24,  null: false
-    t.string   "password",   limit: 128, null: false
+    t.string   "user_name",  limit: 24,              null: false
+    t.string   "password",   limit: 128,             null: false
     t.string   "name",       limit: 128
     t.date     "birthday"
     t.datetime "joined_at"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20170522070129) do
     t.string   "last_login", limit: 45
     t.integer  "level",      limit: 1
     t.integer  "manager_id"
+    t.integer  "status",     limit: 1,   default: 0
+    t.string   "job",        limit: 256
+    t.string   "phone",      limit: 45
     t.index ["user_name"], name: "user_name_UNIQUE", unique: true, using: :btree
   end
 
